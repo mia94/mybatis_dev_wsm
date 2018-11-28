a3191463b596d076451478dc567159c883f08e5b
mia94 <mia_3@hanmail.net>
2018-11-28 17:18:19 +0900

    mybatis initialize complete

---
+++ src/test/java/kr/or/yi/mybatis_dev_wsm/MyBatisSqlSessionFactoryTest.java
@@ -0,0 +1,25 @@
+package kr.or.yi.mybatis_dev_wsm;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+
+import kr.or.yi.mybatis_dev_wsm.jdbc.MyBatisSqlSessionFactory;
+
+public class MyBatisSqlSessionFactoryTest {
+	
+	private static final Log log = LogFactory.getLog(MyBatisSqlSessionFactoryTest.class);
+
+
+	@Test
+	public void testOpenSession() {
+		log.debug("testOpenSession()");
+		
+		SqlSession session = MyBatisSqlSessionFactory.openSession();
+		log.debug("session "+session);
+		Assert.assertNotNull(session);
+	}
+
+}
