ce2b6ff0127b260f6da8d9a42bab04918acf7877
mia94 <mia_3@hanmail.net>
2018-11-29 14:27:58 +0900

    dto추가
    phoneNumber, Student

---
+++ src/main/java/kr/or/yi/mybatis_dev_wsm/dto/PhoneNumber.java
@@ -0,0 +1,60 @@
+package kr.or.yi.mybatis_dev_wsm.dto;
+
+public class PhoneNumber {
+	private String contryCode;
+	private String stateCode;
+	private String number;
+	public PhoneNumber() {
+		// TODO Auto-generated constructor stub
+	}
+	public PhoneNumber(String contryCode, String stateCode, String number) {
+		this.contryCode = contryCode;
+		this.stateCode = stateCode;
+		this.number = number;
+	}
+
+/**
+ * @author str "000-0000-0000" 여기서 - 는 시그니처
+ *
+ */
+	public PhoneNumber(String str) {
+		if(str != null) {
+			String[] parts = str.split("-");
+			
+			if(parts.length>0) this.contryCode = parts[0];
+			if(parts.length>1) this.stateCode = parts[1];
+			if(parts.length>2) this.number = parts[2];
+		}
+	}
+	
+	public String getContryCode() {
+		return contryCode;
+	}
+
+	public void setContryCode(String contryCode) {
+		this.contryCode = contryCode;
+	}
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s-%s-%s", contryCode, stateCode, number);
+	}
+	
+	
+}
